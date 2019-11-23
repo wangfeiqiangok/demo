8c0e50100b9c7b381c5f437534b26c6d81ab0622
wangfeiiqangok <wangfeiqiangok@126.com>
2019-11-23 12:50:02 +0800

    修改11222fff

--- src/main/java/com/pojo/User.java
+++ src/main/java/com/pojo/User.java
@@ -2,7 +2,7 @@
 
 public class User {
     private  String usrgergethtrhername;//用户名
-    private  int aefewrgregge;//年龄99988
+    private  int aefewrgreggfffe;//年龄99988
     private String passwordgrththjytj;
     private  String ui56u54u46username;//用户名
     private  int aghrthrjhtrje;//年龄99988
